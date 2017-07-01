f60609596fccd446fddc2e21444ed4dd788b09d2
yaw joseph etse <yaw.etse@gmail.com>
2017-06-30 23:42:33 -0400

    added extension tests

--- test/unit/periodic_extension_spec.js
+++ test/unit/periodic_extension_spec.js
@@ -9,14 +9,58 @@ const sinon = require('sinon');
 const semver = require('semver');
 const expect = require('chai').expect;
 const extension_root_dir = path.resolve(__dirname, '../../');
-let extension_files=[];
+let extension_files = [];
 chai.use(require('sinon-chai'));
 require('mocha-sinon');
 const extensionJSON = require(path.join(extension_root_dir, 'periodicjs.ext.json'));
+const extensionPACKAGE_JSON = require(path.join(extension_root_dir, 'package.json'));
 
-describe('Valid Periodic extension', function () {
+// const periodicClass = require('periodicjs/lib/periodicClass');
+const testPathDir = path.resolve(__dirname, '../mock/spec/periodic');
+const initTestPathDir = path.join(testPathDir, 'EXT_TEST');
+// const initTestConfigJsonFile = path.join(initTestPathDir, 'content/config/config.json');
+// const extTestConfigJson = require('../../mock/config/config_test_config');
+let extPeriodic = require('periodicjs');
+
+describe('Valid Periodic extension', function() {
   this.timeout(10000);
-  describe('complete extension folder structure', function () {
+  before('initialize extension test periodic dir', (done) => {
+    Promise.all([
+        fs.ensureDir(initTestPathDir),
+        fs.ensureDir(path.join(initTestPathDir, 'node_modules'))
+      ])
+      .then(() => {
+        // extPeriodic = new periodicClass({});
+        // console.log('extPeriodic.init', extPeriodic.init)
+        extPeriodic.init({
+            app_root: initTestPathDir,
+            cli: true,
+            environment: 'test',
+          })
+          .then(() => {
+            return fs.copy(extension_root_dir, path.join(initTestPathDir, 'node_modules', extensionPACKAGE_JSON.name), {
+              filter: (src, dest) => {
+                if (src.indexOf(path.join(extension_root_dir, 'test')) === -1 &&
+                  src.indexOf(path.join(extension_root_dir, 'node_modules')) === -1 &&
+                  src.indexOf(path.join(extension_root_dir, 'node_modules', extensionPACKAGE_JSON.name, '__LEGACY')) === -1) {
+                  return true;
+                } else {
+                  return false;
+                }
+              },
+            });
+          })
+          .then(() => {
+            return extPeriodic.crud.ext.create(extensionPACKAGE_JSON.name);
+          })
+          .then(result => {
+            console.log({ result });
+            done();
+          })
+          .catch(done);
+      }).catch(done);
+  });
+  describe('complete extension folder structure', function() {
     it('should have all of the required extension components', (done) => {
       const standardExtensionStructure = [
         'commands',
@@ -52,7 +96,7 @@ describe('Valid Periodic extension', function () {
       if (extension_files.indexOf('commands') > -1) {
         const EXTENSION_commands = require(path.join(extension_root_dir, 'commands/index'));
         Object.keys(EXTENSION_commands).forEach(command => {
-          expect(EXTENSION_commands[ command ]).to.be.a('function');
+          expect(EXTENSION_commands[command]).to.be.a('function');
         });
       } else {
         console.log('Extension has no commands');
@@ -68,8 +112,8 @@ describe('Valid Periodic extension', function () {
       expect(EXTENSION_utilities).to.be.an('object');
     });
     it('should export a valid express router', () => {
-      if(extension_files.indexOf('routers') >-1 ){
-        const EXTENSION_routers = require(path.join(extension_root_dir,   'routers/index'));
+      if (extension_files.indexOf('routers') > -1) {
+        const EXTENSION_routers = require(path.join(extension_root_dir, 'routers/index'));
         expect(EXTENSION_routers).to.be.a('function');
         expect(EXTENSION_routers).to.have.a.property('stack');
         expect(EXTENSION_routers).to.have.a.property('params');
@@ -82,27 +126,27 @@ describe('Valid Periodic extension', function () {
         const EXTENSION_transforms = require(path.join(extension_root_dir, 'transforms/index'));
         const flattened_transforms = flatten(EXTENSION_transforms);
         Object.keys(flattened_transforms).forEach(transform => {
-          if (Object.keys(flattened_transforms[ transform ]).length) {
-            expect(flattened_transforms[ transform ]).to.be.a('function');
+          if (Object.keys(flattened_transforms[transform]).length) {
+            expect(flattened_transforms[transform]).to.be.a('function');
           }
         });
       } else {
         console.log('Extension has no transforms');
       }
     });
-    it('should export a valid node module',()=>{
+    it('should export a valid node module', () => {
       const EXTENSION_module = require(path.join(extension_root_dir, 'index'));
       expect(EXTENSION_module).to.be.a('function');
     });
   });
-  describe('periodicjs.ext.json', function () { 
+  describe('periodicjs.ext.json', function() {
     it('should declare periodic compatibility', () => {
       expect(semver.valid(extensionJSON.periodic_compatibility)).to.be.ok;
       expect(extensionJSON.periodic_compatibility).to.be.a('string');
     });
     it('should have a valid periodic_type', () => {
       // console.log('Extension periodicjs.ext.json periodic_type classification (0-core, 1-communication, 2-auth, 3-uac, 4-api, 5-admin,6-data,7-ui)');
-      expect(extensionJSON.periodic_type).to.be.oneOf([ 0, 1, 2, 3, 4, 5, 6, 7 ]);
+      expect(extensionJSON.periodic_type).to.be.oneOf([0, 1, 2, 3, 4, 5, 6, 7]);
     });
     it('should have a valid periodic_priority', () => {
       expect(extensionJSON.periodic_priority).to.be.a('number');
@@ -111,4 +155,10 @@ describe('Valid Periodic extension', function () {
       expect(extensionJSON.periodic_config).to.be.a('object');
     });
   });
+  // after('remove extension test periodic dir', (done) => {
+  //   fs.remove(initTestPathDir)
+  //     .then(() => {
+  //       done();
+  //     }).catch(done);
+  // });
 });
